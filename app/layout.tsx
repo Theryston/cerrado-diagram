90362091afed4d42f3b85fab693e5347c7bd376e
Theryston <devtheryston@gmail.com>
2025-03-03 17:30:09 -0300

    feat: gtm id

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -22,6 +23,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GMT_ID || ""} />
+
       <body className={clsx(inter.className, "antialiased")}>{children}</body>
     </html>
   );
